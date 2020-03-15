5f9f49f8991a3ae9b69e062cab90d3af74e72130
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-15 23:41:08 +0200

    feat(gatsby-plugin-intl): add `notFoundPage` and `excludedPages` option

--- packages/gatsby-plugin-intl/src/constants.js
+++ packages/gatsby-plugin-intl/src/constants.js
@@ -1,6 +1,17 @@
 export const DEFAULT_OPTIONS = {
-  supportedLanguages: ['en', 'el'],
+  // the locales that your gatsby app supports
+  supportedLanguages: ['en'],
+
+  // the default language for your gatsby app
   defaultLanguage: 'en',
-  notFoundPages: ['/404/', '/404.html'],
+
+  // the domain (url) in which your web app is hosted in
   siteUrl: process.env.URL || 'localhost:8000',
+
+  // the (optional) path in which your web app redirects in case of a 404
+  notFoundPage: '/404/',
+
+  // a list of paths, whose pages should not be processed by this plugin (i.e. delegate to whatever
+  // gatsby would do by default)
+  excludedPages: ['/404.html'],
 };
--- packages/gatsby-plugin-intl/src/gatsby-node.js
+++ packages/gatsby-plugin-intl/src/gatsby-node.js
@@ -5,17 +5,23 @@
  */
 import { DEFAULT_OPTIONS } from './constants';
 
+const isEnvDevelopment = process.env.NODE_ENV === 'development';
+
 export const onCreatePage = async (
   { page, actions: { createPage, deletePage, createRedirect } },
   pluginOptions
 ) => {
-  const { siteUrl, supportedLanguages, defaultLanguage, notFoundPages } = {
+  const { siteUrl, supportedLanguages, defaultLanguage, notFoundPage, excludedPages } = {
     ...DEFAULT_OPTIONS,
     ...pluginOptions,
   };
-  const isEnvDevelopment = process.env.NODE_ENV === 'development';
   const originalPath = page.path;
-  const is404 = notFoundPages.some(p => originalPath.includes(p));
+  const is404 = originalPath.includes(notFoundPage);
+
+  // return early if page is exluded
+  if (excludedPages.includes(originalPath)) {
+    return;
+  }
 
   // Delete the original page (since we are gonna create localized versions of it) and add a
   // redirect header
@@ -27,7 +33,7 @@ export const onCreatePage = async (
 
       // create a redirect based on the accept-language header
       createRedirect({
-        fromPath: is404 ? `/${lang}/*` : originalPath,
+        fromPath: originalPath,
         toPath: localizedPath,
         Language: lang,
         isPermanent: false,
@@ -52,10 +58,28 @@ export const onCreatePage = async (
   // Create a fallback redirect if the language is not supported or the
   // Accept-Language header is missing for some reason
   createRedirect({
-    fromPath: is404 ? '/*' : originalPath,
+    fromPath: originalPath,
     toPath: `/${defaultLanguage}${page.path}`,
     isPermanent: false,
     redirectInBrowser: isEnvDevelopment,
     statusCode: is404 ? 404 : 301,
   });
+};
+
+exports.onPreBuild = ({ actions: { createRedirect } }, pluginOptions) => {
+  const { notFoundPage } = { ...DEFAULT_OPTIONS, ...pluginOptions };
+
+  // we add a generic redirect to the "not found path" for every path that's not present in the app.
+  // This rule needs to be the last one (so that it only kicks in if nothing else matched before),
+  // thus it's added after all the page-related hooks have finished (hence "onPreBuild")
+
+  if (notFoundPage) {
+    createRedirect({
+      fromPath: '/*',
+      toPath: notFoundPage,
+      isPermanent: false,
+      redirectInBrowser: isEnvDevelopment,
+      statusCode: 302,
+    });
+  }
 };
