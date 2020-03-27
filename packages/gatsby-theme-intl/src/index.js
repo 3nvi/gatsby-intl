99068827b0fb040b66a726962c5888b6d676138e
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-27 10:23:43 +0200

    feat(gatsby-theme-plugin): reduce the amount of exported modules

--- packages/gatsby-theme-intl/src/index.js
+++ packages/gatsby-theme-intl/src/index.js
@@ -1,5 +1,4 @@
 export { default as Link } from './components/link';
-export * from './page-context';
+export { usePageContext } from './page-context';
 
-export { default as i18next } from 'i18next';
 export * from 'react-i18next';
