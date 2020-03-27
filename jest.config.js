d5451187138e7cce2222d42bdfdf7539ea213465
Aggelos Arvanitakis <agg.arvanitakis@gmail.com>
2020-03-27 21:46:34 +0200

    fix: description

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     '^.+\\.jsx?$': '<rootDir>/jest-preprocess.js',
   },
-  testPathIgnorePatterns: ['node_modules'],
+  testPathIgnorePatterns: ['node_modules', '.cache'],
   transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
   globals: {
     __PATH_PREFIX__: '',
